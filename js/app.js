4250be436dd7119c9de4ec325392b235f4323244
Arjan de Ruijter <rra@mboutrecht.nl>
2019-10-11 12:10:14 +0200

    Test na test

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // alert("Dit is mijn eerste typescriptproject!!");
+function show_winner() {
+    let carou_01;
+    let img_01;
+    carou_01 = document.querySelector("#carouselExampleSlidesOnly");
+    img_01 = document.querySelector("#img_01");
+    carou_01.style.display = "none";
+    img_01.setAttribute("src", "./img/ferguson_vs_pettis.png");
+    img_01.style.display = "block";
+}
--- ts/app.ts
+++ ts/app.ts
@@ -1,1 +1,12 @@
 // alert("Dit is mijn eerste typescriptproject!!");
+function show_winner(): void {
+  let carou_01: HTMLDivElement;
+  let img_01: HTMLImageElement;
+
+  carou_01 = document.querySelector("#carouselExampleSlidesOnly");
+  img_01 = document.querySelector("#img_01");
+
+  carou_01.style.display = "none";
+  img_01.setAttribute("src", "./img/ferguson_vs_pettis.png");
+  img_01.style.display = "block";
+}
